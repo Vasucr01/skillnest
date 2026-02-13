d818ddae8abbe17b5eca3956cbecc669bd5c3f73
kevalchauhan <vasuchauhan202020@gmail.com>
2026-02-13 17:33:23 +0530

    Trigger automatic admin creation on first request

--- skillnest/wsgi.py
+++ skillnest/wsgi.py
@@ -8,10 +8,18 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'skillnest.settings')
 
+# AUTOMATIC SETUP (Runs on Vercel startup)
+# This will run migrations and create your admin account on the first visit
+from django.core.management import execute_from_command_line
+try:
+    execute_from_command_line(['manage.py', 'migrate', '--noinput'])
+    execute_from_command_line(['manage.py', 'create_admin'])
+except Exception as e:
+    print(f"Startup setup: {e}")
+
 application = get_wsgi_application()
-app=application
+app = application
